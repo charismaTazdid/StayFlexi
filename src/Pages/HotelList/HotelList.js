a79e8e8793fe7d4b524038aa2a6df37da9f22afc
raihan tajdid <raihantajdid@gmail.com>
2023-03-17 09:52:20 +0600

    hotellist

--- src/Pages/HotelList/HotelList.js
+++ src/Pages/HotelList/HotelList.js
@@ -1,9 +1,18 @@
+import { format } from "date-fns";
+import { useState } from "react";
+import { useLocation } from "react-router-dom";
 import Header from "../../components/Header/Header";
 import Navbar from "../../components/Navbar/Navbar";
 import styles from "./hotlelList.module.css";
 
 
 const HotelList = () => {
+    const location = useLocation();
+
+    const [destination, setDestination] = useState(location.state.destination)
+    const [date, setDate] = useState(location.state.date)
+    const [options, setOptions] = useState(location.state.options)
+
     return (
         <div>
             <Navbar />
@@ -16,11 +25,13 @@ const HotelList = () => {
                         <h1 className={styles.title}>Search</h1>
                         <div className={styles.item}>
                             <label htmlFor="">Destination</label>
-                            <input type="text" />
+                            <input type="text" placeholder={destination} />
                         </div>
                         <div className={styles.item}>
                             <label htmlFor="">Check in Date</label>
-
+                            <span>
+                                {`${format(date[0].startDate, "MM/dd/yyyy")} to ${format(date[0].endDate, "MM/dd/yyyy")}`}
+                            </span>
                         </div>
                     </div>
                     <div className={styles.listResult}>
