5a9ee568dd3a6601887333c752101ac472d9a09b
raihan tajdid <raihantajdid@gmail.com>
2023-03-23 05:10:46 +0600

    search Itme

--- src/Pages/HotelList/HotelList.js
+++ src/Pages/HotelList/HotelList.js
@@ -4,6 +4,7 @@ import { DateRange } from "react-date-range";
 import { useLocation } from "react-router-dom";
 import Header from "../../components/Header/Header";
 import Navbar from "../../components/Navbar/Navbar";
+import SearchItems from "../../components/SearchItems/SearchItems";
 import styles from "./hotlelList.module.css";
 
 
@@ -12,7 +13,8 @@ const HotelList = () => {
 
     // will add Optional Chening later
     const [destination, setDestination] = useState(location.state.destination)
-    const [date, setDate] = useState(location.state.date)
+    const [date, setDate] = useState(location.state.date);
+    const [showCalender, setShowCalender] = useState(false);
     const [options, setOptions] = useState(location.state.options)
 
     return (
@@ -31,20 +33,64 @@ const HotelList = () => {
                         </div>
                         <div className={styles.item}>
                             <label htmlFor="">Check in Date</label>
-                            <span>
+                            <span onClick={() => setShowCalender(!showCalender)}>
                                 {`${format(date[0]?.startDate, "MM/dd/yyyy")} to ${format(date[0]?.endDate, "MM/dd/yyyy")}`}
                             </span>
 
-                            <DateRange
-                                onChange={(item) => setDate([item.selection])}
-                                minDate={new Date()}
-                                ranges={date}
-                            />
+                            {
+                                showCalender && <DateRange
+                                    onChange={(item) => setDate([item.selection])}
+                                    minDate={new Date()}
+                                    ranges={date}
+                                />
+                            }
 
                         </div>
-                    </div>
-                    <div className={styles.listResult}>
+                        {/* Options */}
+                        <div className={styles.item}>
+                            <label htmlFor=""> Options </label>
 
+                            <div className={styles.options}>
+
+                                <div className={styles.optionItem}>
+                                    <span className={styles.optionText}> Min Price <small> Per Night</small>  </span>
+                                    <input type="number" className={styles.optionInput} />
+                                </div>
+
+                                <div className={styles.optionItem}>
+                                    <span className={styles.optionText}> Max Price <small> Per Night</small> </span>
+                                    <input type="number" className={styles.optionInput} />
+                                </div>
+
+                                <div className={styles.optionItem}>
+                                    <span className={styles.optionText}> Adult  </span>
+                                    <input type="number" className={styles.optionInput} placeholder={options.adult} min={0} />
+                                </div>
+
+                                <div className={styles.optionItem}>
+                                    <span className={styles.optionText}> Children </span>
+                                    <input type="number" className={styles.optionInput} placeholder={options.children} min={0} />
+                                </div>
+
+                                <div className={styles.optionItem}>
+                                    <span className={styles.optionText}> Room </span>
+                                    <input type="number" className={styles.optionInput} placeholder={options.room} min={1} />
+                                </div>
+
+                            </div>
+                        </div>
+                        <button className={styles.btn}>Search</button>
+                    </div>
+
+
+                    <div className={styles.listResult}>
+                        {/* Have Created another Component for Readability */}
+                        <SearchItems />
+                        <SearchItems />
+                        <SearchItems />
+                        <SearchItems />
+                        <SearchItems />
+                        <SearchItems />
                     </div>
                 </div>
             </div>
---
+++ src/components/SearchItems/SearchItems.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import styles from "./SearchItems.module.css";
+
+
+const SearchItems = () => {
+    return (
+        <div className={styles.searchItemContainer}>
+
+            <img src="https://t-cf.bstatic.com/xdata/images/hotel/square600/13125860.webp?k=e148feeb802ac3d28d1391dad9e4cf1e12d9231f897d0b53ca067bde8a9d3355&o=&s=1" alt="" className={styles.image} />
+            <div className={styles.description}>
+
+            </div>
+            <div className={styles.details}>
+
+            </div>
+        </div>
+    );
+};
+
+export default SearchItems;
